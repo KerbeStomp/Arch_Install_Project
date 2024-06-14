df535a8819ec9d810fe3101757e65182df958180
Jacob Perez <34731980+KerbeStomp@users.noreply.github.com>
2024-06-14 10:47:39 +0000

    Missing functions now exits installer

--- entry.sh
+++ entry.sh
@@ -12,14 +12,14 @@ log_proc(){
     log "Stage: ${proc_name}"
     "$proc_func"
     local stat=$?
-    if [[ "$stat" == 1 ]]; then
-        log "Fatal error encounted in ${proc_func}" 2 1
+    if [[ "$stat" == 1 || "$stat" == 127 ]]; then
+        log "$(pad "Fatal error encounted in ${proc_func}...")" 2
         return 1
     elif [[ "$stat" == 2 ]]; then
-        log "Non-fatal error encountered in ${proc_func}..." 3 1
+        log "$(pad "Non-fatal error encountered in ${proc_func}...")" 3
         return 2
     else
-	log "Successfully exited ${proc_func}" 1 1
+        log "$(pad "Successfully exited ${proc_func}")" 1
 	return 0
     fi
 
@@ -35,18 +35,17 @@ entry(){
     local proc_func=("set_font" "set_kb" "verify_boot" "set_net" "set_clk"\
         "part_disk" "fmt_disk" "mnt_disk" "inst_pkgs" "cfg_sys" "bootldr")
 
-    prt_con ""
-    log "Starting Arch Install"
-    prt_con ""
+    log "Starting Arch installation"
+    log ""
 
-    for proc in {0..1}; do
-        log_proc "${proc_name[${proc}]}" "${proc_func[${proc}]}"
-        local proc_stat=$?
-        prt_con ""
+    for proc in {0..10}; do
+	    log_proc "${proc_name[${proc}]}" "${proc_func[${proc}]}"
+	    local proc_stat=$?
+        log ""
 
-        if [[ proc_stat -eq 1 ]]; then
+        if [[ "$proc_stat" == 1 ]]; then
             log "Error during installation..." 2
-            prt_con ""
+            log ""
             return 1
         fi
     done
