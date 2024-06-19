b2380f8140084c77a41efdc07cd94add430579e6
Jacob <34731980+KerbeStomp@users.noreply.github.com>
2024-06-19 08:26:08 +0000

    Finished clock process

--- proc/system_clock.sh
+++ proc/system_clock.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# update system clock
-update_clock() {
-    if timedatectl set-ntp true; then
-        return CLK_OK 
+# set_clk: sets the systemclock
+set_clk()
+{
+	timedatectl > /dev/null 2>&1
+	clk_stat=$?
+    if [[ "$clk_stat" == 0 ]]; then
+	    log "$(pad "Set system clock")" 1
+        return 0 
     else
-        return CLK_ERR
+	    log "$(pad "Unable to set system clock...")" 3
+        return 1
     fi
 }
